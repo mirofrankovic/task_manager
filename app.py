a1f543d26e8a66144676ffed804e6f27d661fb8f
Miro Frankovic <miro.frankovic@gmail.com>
2019-01-20 11:36:59 +0000

    Added templates and modified database

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = 'mongodb://mirof:Vanilia123@ds261644.mlab.com:61644/task_manager'
 
-@app.route("/")
-def hello():
-    return "Hello World...again"
+mongo = PyMongo(app)
+
+@app.route('/')
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html",
+    tasks=mongo.db.tasks.find())
+
+
+#@app.route('/')
+#def hello():
+    #return "Hello World...again"
     
     
 if __name__  ==  "__main__":
     app.run(host=os.environ.get('IP'),
-    port=int(os.environ.get('PORT')),
-    debug=True)
+        port=int(os.environ.get('PORT')),
+        debug=True)
