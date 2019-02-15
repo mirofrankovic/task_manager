09000bc4a5a36972a6d0e80d432374b4e6fd72d4
Miro Frankovic <miro.frankovic@gmail.com>
2019-02-15 11:22:08 +0000

    modified task functionalities

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def add_task():
     return render_template("addtask.html",
     categories=mongo.db.categories.find())
     
-@app.route('/insert_task', methods=['POST'])
+@app.route('/insert_task', methods=["POST"])
 def insert_task():
     tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
@@ -31,6 +31,24 @@ def edit_task(task_id):
     the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task, categories=all_categories)
+    
+@app.route('/update_task<task_id>', methods=["POST"]) 
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+     'task_name':request.form.get['task_name'],
+     'category_name':request.form.get['category_name'],
+     'task_description':request.form.get['task_description'],
+     'due_date':request.form.get['due_date'],
+     'is_urgent':request.form.get['is_urgent']
+    })
+    return redirect(url_for('get_tasks'))
+    
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
 
     
 if __name__  ==  "__main__":
